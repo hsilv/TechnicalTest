515bcbb3fbcbfe87e21b57670dceb5266eb149a3
hsilv <hm.sebastiansilva@gmail.com>
2024-08-22 02:19:50 -0600

    [feat/backend]: Prevent execute migrations unnecesary

--- backend/migrations/Version20240822081823.php
+++ backend/migrations/Version20240822081823.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240822081244 extends AbstractMigration
+final class Version20240822081823 extends AbstractMigration
 {
     public function getDescription(): string
     {
