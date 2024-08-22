73d971360970149fb1ca77564ea6c6c6017d4c20
hsilv <hm.sebastiansilva@gmail.com>
2024-08-21 19:26:10 -0600

    [feat/devops]: Implement Docker between Symfony, PostgreSQL y Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -37,4 +37,12 @@ export default defineConfig({
       scss: {},
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
+    },
+  },
 });
