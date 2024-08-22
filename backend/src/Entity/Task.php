e77130259cffa4c21f79dc55c66759ef69a1971b
hsilv <hm.sebastiansilva@gmail.com>
2024-08-21 23:11:37 -0600

    [feat/backend]: Task CRUD and Migrations

---
+++ backend/src/Entity/Task.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ApiResource]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: "The title is required should not be blank.")]
+    private $title;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank(message: "The description is required or should not be blank.")]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
